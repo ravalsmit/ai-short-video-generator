3214cdab028970cb2acd8acb8e6b994590ee6aff
ravalsmit <ravalsmit780@gmail.com>
2024-10-15 17:38:42 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+}); 
 
 export const metadata = {
   title: "Create Next App",
