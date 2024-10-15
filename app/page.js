e7519fb501f50dc0cf345dbc5fcb5f03ddb501ba
ravalsmit <ravalsmit780@gmail.com>
2024-10-15 18:03:19 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <div>
       <h2>Subscribe to TubeGuruji</h2>
+      <Button variant="destructive">Subscribe</Button>
     </div>
   );
 }
---
+++ configs/schema.js
@@ -0,0 +1,10 @@
+import { varchar, serial, boolean } from "drizzle-orm/mysql-core";
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const Users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name').notNull(),
+  email: varchar('email').notNull(),
+  imageUrl: varchar('imageUrl'),
+  subscription: boolean('subscription').default(false)
+})
